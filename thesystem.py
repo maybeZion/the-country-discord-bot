b6ab5011a371ec50bc8109d4db6317135c8a3584
maybeZion <ezion@protonmail.com>
2020-06-23 15:58:29 -0700

    First release, probably broken somewhere

---
+++ thesystem.py
@@ -0,0 +1,359 @@
+import discord
+from discord.ext import commands
+from pretty_help import PrettyHelp
+import os
+import json
+import jsonpickle
+import time
+import random
+
+bot = commands.Bot(command_prefix='./', help_command=PrettyHelp())
+
+def save(data, name):
+    with open(name, 'w', encoding='utf-8') as f:
+        try:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+        except TypeError:
+            json.dump(jsonpickle.encode(data), f, ensure_ascii=False, indent=4)
+        print('Saved value "{}" to {}'.format(data, name))
+
+def load(data, name, decode):
+    try:
+        with open(str(name), 'r', encoding='utf-8') as f:
+            if decode:
+                data = jsonpickle.decode(json.load(f))
+            else:
+                data = json.load(f)
+            print('loaded data from "{}" to {}'.format(name, data))
+    except FileNotFoundError:
+        print('File "{}" not found.'.format(name))
+    if str(type(data)) != '<class \'NoneType\'>':
+        return data
+    else:
+        pass
+
+@bot.event
+async def on_ready():
+    print('Logged in.\n----------')
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="./help"))
+
+@bot.event
+async def on_member_join(member):
+    await member.send("Welcome to The Country, {}. ".format(member.name))
+
+class Economy(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+        self.job1 = Job('factory', 3, 0, [''], 100, {'Citizen': True, 'Vagrant': True, 'Cabinet': True})
+        self.job1 = load(self.job1, self.job1.title, True)
+        self.job2 = Job('restaurant', 10, 0.2, [''], 50, {'Citizen': True, 'Vagrant': True, 'Cabinet': True})
+        self.job2 = load(self.job2, self.job2.title, True)
+        self.job3 = Job('construction', 100, 0.4, [''], 25, {'Citizen': True, 'Vagrant': True, 'Cabinet': True})
+        self.job3 = load(self.job3, self.job3.title, True)
+        self.job4 = Job('office', 400, 0.6, [''], 12, {'Citizen': True, 'Vagrant': True, 'Cabinet': True})
+        self.job4 = load(self.job4, self.job4.title, True)
+        self.job5 = Job('farm', 800, 0.8, [''], 6, {'Citizen': True, 'Vagrant': True, 'Cabinet': True})
+        self.job5 = load(self.job5, self.job5.title, True)
+        self.jobs = [self.job1, self.job2, self.job3, self.job4, self.job5]
+        self.ledger = {}
+        self.ledger = load(self.ledger, 'ledger', False)
+        self.clockin = {}
+        self.clockin = load(self.clockin, 'clockin', False)
+
+    @commands.command()
+    async def bal(self, ctx):
+        try:
+            await ctx.send('Your current balance is **{} credits**.'.format(self.ledger[str(ctx.message.author.id)]))
+        except KeyError:
+            self.ledger[str(ctx.message.author.id)] = 0
+            await ctx.send('Your current balance is **{} credits**.'.format(self.ledger[str(ctx.message.author.id)]))
+
+    @commands.command()
+    async def work(self, ctx):
+        worked_job = None
+        x = None
+        for item in self.jobs:
+            if ctx.message.author in item.currentworkers:
+                worked_job = item
+                y = worked_job.jobpay() * item.tax
+        if worked_job is not None:
+            try:
+                if self.clockin[str(ctx.message.author.id)] <= time.time() - 3600:
+                    x = True
+                    self.clockin[str(ctx.message.author.id)] = time.time()
+            except KeyError:
+                self.clockin[str(ctx.message.author.id)] = time.time()
+            if x:
+                try:
+                    self.ledger[str(ctx.message.author.id)] += y
+                except KeyError:
+                    self.ledger[str(ctx.message.author.id)] = y
+                save(worked_job, worked_job.title)
+                await ctx.send('You made **{} credits** working!'.format(y))
+            else:
+                await ctx.send('Please wait another {} minute(s) before starting your shift.'.format(int((self.clockin[str(ctx.message.author.id)] - (time.time() - 3600)) // 60)))
+        else:
+            await ctx.send('It seems you have no job. Try `./job search`.')
+        save(self.ledger, 'ledger')
+        save(self.clockin, 'clockin')
+
+    @commands.group()
+    async def job(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send('Your command seems incomplete; try `./help job`')
+
+    @commands.group()
+    async def edit(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send('Your command seems incomplete; try `./help job`')
+        else:
+            for role in ctx.message.author.roles:
+                if role.name == 'Leader':
+                    x = True
+            if x:
+                self.temp_leader_bool = True
+
+    @edit.command()
+    async def title(self, ctx, job, title):
+        if self.temp_leader_bool:
+            for job in self.jobs:
+                if job.title == job:
+                    job.title = title
+                    save(job, job.title)
+        else:
+            await ctx.send("Only the current leader can use this command.")
+    
+    @edit.command()
+    async def pay(self, ctx, job, pay):
+        if self.temp_leader_bool:
+            for job in self.jobs:
+                if job.title == job and job.pay >= 10 * job.maxworkers:
+                    job.pay = pay
+                    save(job, job.title)
+        else:
+            await ctx.send("Only the current leader can use this command.")
+
+    @edit.command()
+    async def positions(self, ctx, job, maxworkers):
+        if self.temp_leader_bool:
+            for job in self.jobs:
+                if job.title == job and job.maxworkers >= job.pay / 10:
+                    job.maxworkers = maxworkers
+                    while len(job.currentworkers) > job.maxworkers:
+                        job.currentworkers.pop()
+                        x += 1
+                    save(job, job.title)
+                else:
+                    await ctx.send('You misspelled the job title, or your parameters are out of range. The amount of workers can only be one-tenth of job\'s pay.')
+        else:
+            await ctx.send("Only the current leader can use this command.")
+
+    @edit.command()
+    async def tax(self, ctx, job, tax):
+        if self.temp_leader_bool:
+            for job in self.jobs:
+                if job.title == job and tax < .5:
+                    self.tax = tax
+                    save(job, job.title)
+        else:
+            await ctx.send("Only the current leader can use this command.")
+
+    @edit.command()
+    async def workers(self, ctx, job, workertype, truefalse):
+        if self.temp_leader_bool:
+            for job in self.jobs:
+                if job.title == job and type(truefalse) == bool:
+                    if workertype == 'citizen':
+                        job.typeworkers['Citizen'] = truefalse
+                    elif workertype == 'vagrant':
+                        job.typeworkers['Vagrant'] = truefalse
+                    elif workertype == 'cabinet':
+                        job.typeworkers['Cabinet'] = truefalse
+                    save(job, job.title)
+        else:
+            await ctx.send("Only the current leader can use this command.")
+
+    @job.command()
+    async def search(self, ctx):
+        x = []
+        availablejobs = 0
+        for job in self.jobs:
+            availablejobs += job.maxworkers - len(job.currentworkers)
+            x.append(job.title)
+        availablejobs = availablejobs - len(job.currentworkers)
+        x = '`, `'.join(x)
+        await ctx.send('There are currently **{}** available jobs, spread over **{}** positions:\n`{}`\nDo `./job apply <position>`.'.format(availablejobs, len(self.jobs), x))
+
+    @job.command()
+    async def apply(self, ctx, applied_job):
+        last_job = None
+        for item in ctx.message.author.roles:
+            if item.name == 'Citizen':
+                citizentype_temp = item
+            elif item.name == 'Vagrant':
+                citizentype_temp = item
+            elif item.name == 'Cabinet':
+                citizentype_temp = item
+        for item in self.jobs:
+            if ctx.message.author.id in item.currentworkers:
+                last_job = item
+            if applied_job == item.title:
+                applied_job = item
+        if type(applied_job) == str:
+            await ctx.send('That job doesn\'t exist. Please do `./job search`')
+        elif len(applied_job.currentworkers) == applied_job.maxworkers:
+            await ctx.send('There are currently no vacancies in this position. Please do `./job search, or try again later.`')
+        elif applied_job.typeworkers[citizentype_temp.name]:
+            try:
+                last_job.currentworkers.remove(ctx.message.author.id)
+                save(last_job, last_job.title)
+            except AttributeError:
+                pass
+            applied_job.currentworkers.append(ctx.message.author.id)
+            await ctx.send('Congratulations, you have a job! You can now do `./job work` to work once per hour.')
+        else:
+            await ctx.send('Sorry, you aren\'t eligible for this position.')
+        save(applied_job, applied_job.title)
+
+class Moderation(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+    
+    @commands.command()
+    async def purge(self, ctx, count : int):
+        messages = []
+        for role in ctx.message.author.roles:
+            if role.name == 'Admin':
+                x = True
+                break
+        if x is None:
+            await ctx.send("You don't have permission to use this command.")
+        else:
+            try:
+                async for message in ctx.message.channel.history(limit=count):
+                    messages.append(message)
+                await ctx.message.channel.delete_messages(messages)
+                x = await ctx.send('Purged **{}** messages.'.format(count))
+                time.sleep(4)
+                await x.delete()
+            except TypeError:
+                await ctx.send('Please specify the number of messages you want purged (100 max)')
+
+class Party(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+        self.pending = {}
+        self.pending = load(self.pending, 'pending', False)
+
+    @commands.group()
+    async def party(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send('Your command seems incomplete; try `./help party`')
+
+    @party.command()
+    async def join(self, ctx, party : str):
+        x = None
+        for item in ctx.message.author.roles:
+            if 'Party' in item.name:
+                await ctx.send('You are already in a party. Do `./party leave` before running this command.')
+                x = True
+                break
+        if x is None:
+            if party in 'redyellowbluegreen':
+                self.pending[ctx.message.author.mention] = party
+                await ctx.send('You have been added to the party\'s waiting list.')
+                save(self.pending, 'pending')
+            else:
+                await ctx.send('That party doesn\'t exist. Your options are `red`, `yellow`, `blue`, and `green`.')
+        else:
+            pass
+
+    @party.command()
+    async def leave(self, ctx):
+        x = None
+        for item in ctx.message.author.roles:
+            if item.name != 'Party Rep' and 'Party' in item.name:
+                await ctx.message.author.remove_roles(item)
+                await ctx.send('You have been removed from your party. Do `./party join <party>` to join a new one.')
+                x = True
+        if x is None:
+            await ctx.send('You aren\'t in a party. Do `./party join <party>`.')
+
+    @party.command()
+    async def pending(self, ctx):
+        pending_members = []
+        for item in ctx.message.author.roles:
+            if 'Rep' in item.name:
+                x = True
+            elif 'Party' in item.name:
+                y = item
+        if x:
+            try:
+                for item in self.pending:
+                    if self.pending[item].capitalize() in y.name:
+                        pending_members.append(item)
+                if not pending_members:
+                    await ctx.send('There are no members waiting to join your party.')
+                else:
+                    await ctx.send('Here is a list of pending party members:\n{}'.format(', '.join(pending_members)))
+            except KeyError:
+                await ctx.send('It seems that this member is not in waiting to join your party.')
+        else:
+            await ctx.send('You do not have permision to use this command.')
+
+    @party.command()
+    async def accept(self, ctx, member : discord.Member):
+        for item in ctx.message.author.roles:
+            if 'Rep' in item.name:
+                x = True
+            elif 'Party' in item.name:
+                y = item
+        if x:
+            try:
+                if self.pending[member.mention].capitalize() in y.name:
+                    await member.add_roles(y)
+                    del self.pending[member.mention]
+                    await ctx.send('{} has joined your party!'.format(member))
+            except KeyError:
+                await ctx.send('It seems that this member is not in waiting to join your party.')
+        else:
+            await ctx.send('You do not have permission to use this command.')
+
+    @party.command()
+    async def kick(self, ctx, member : discord.Member):
+        for item in ctx.message.author.roles:
+            if item.name == 'Party Rep':
+                x = True
+        if ctx.message.author == member:
+            await ctx.send('You can\'t kick yourself out of the party.')
+        elif x:
+            for item in member.roles:
+                if 'Red' in item.name or 'Green' in item.name or 'Blue' in item.name or 'Yellow' in item.name: 
+                    if 'Party' in item.name and item in ctx.message.author.roles:
+                        await member.remove_roles(item)
+                        await ctx.send('{} has been removed from your party.'.format(member.nick))
+        else:
+            await ctx.send('You do not have permission to use this command.')
+
+
+class Job:
+    def __init__(self, title, pay, tax, currentworkers, maxworkers, typeworkers):
+        self.title = title
+        self.pay = pay
+        self.tax = tax
+        self.currentworkers = currentworkers
+        self.maxworkers = maxworkers
+        self.typeworkers = typeworkers
+
+    def jobpay(self):
+        x = random.randint(self.pay - self.pay // 10, self.pay - self.pay // 10)
+        return x
+
+if __name__ == '__main__':
+    bot.add_cog(Economy(bot))
+    bot.add_cog(Moderation(bot))
+    bot.add_cog(Party(bot))
+    bot.run('token')
